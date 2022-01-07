f3166b8ce1e90c89853e62a4d18b634e1d1856e9
Gonçalo Silva <gonalo99@gmail.com>
2022-01-07 18:15:41 +0000

    another commit

--- our_model.m
+++ our_model.m
@@ -9,8 +9,6 @@ constants.g = 9.81; % m/s^2
 %% Load project file
 data = load_project('examples/project-01.json');
 
-data.mission.segments{2}.velocity = 8;
-
 %% Add missing mission segment and vehicle component parameters
 data.mission = build_mission(data.mission);
 data.vehicle = build_vehicle(data.mission, data.vehicle);
@@ -21,7 +19,7 @@ data.vehicle = build_vehicle(data.mission, data.vehicle);
 %% Mission analyses
 data.vehicle = aero_analysis(data.mission, data.vehicle);
 [data.mission, data.vehicle] = mass_analysis(data.mission, data.vehicle, data.energy);
-%data.vehicle = design_space_analysis(data.mission, data.vehicle, data.energy);
+data.vehicle = design_space_analysis(data.mission, data.vehicle, data.energy);
 
 %% Stability analysis
 data.vehicle = stability(data.vehicle, data.mission);
